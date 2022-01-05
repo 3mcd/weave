2a25afe5129f5cc36994ea25dba7890a331f7beb
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-01-05 17:35:03 -0500

    test: wip

---
+++ src/guarantor.spec.ts
@@ -0,0 +1,39 @@
+import { Guarantor } from "./guarantor"
+
+class Positive extends Guarantor<number> {
+  filter(n: number) {
+    return n > 0
+  }
+
+  isInvalid() {
+    return this.latest <= 0
+  }
+}
+
+describe("Guarantor", () => {
+  beforeAll(() => jest.useFakeTimers())
+  afterAll(() => jest.useRealTimers())
+  it("works", () => {
+    const positive = new Positive(0)
+    jest.useFakeTimers()
+    setTimeout(() => positive.update(-10), 0)
+    jest.runAllTimers()
+    setTimeout(() => positive.update(0.1), 1)
+    jest.runAllTimers()
+    return expect(positive.guarantee()).resolves.toBe(0.1)
+  })
+  it("works", async () => {
+    const positive = new Positive(0)
+    jest.useFakeTimers()
+    setTimeout(() => positive.update(-10), 0)
+    jest.runAllTimers()
+    setTimeout(() => positive.update(0.1), 1)
+    jest.runAllTimers()
+    setTimeout(() => positive.update(-1000), 1)
+    jest.runAllTimers()
+    setTimeout(() => positive.update(10), 1)
+    jest.runAllTimers()
+    await expect(positive.guarantee()).resolves.toBe(10)
+    await expect(positive.guarantee()).resolves.toBe(10)
+  })
+})
